2654c38976f42ef99769702944162159eaa6bef0
pedrocaldas42 <64943773+pedrocaldas42@users.noreply.github.com>
2023-12-26 23:53:26 -0300

    Create 7 - Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.py

---
+++ 7 - Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.py
@@ -0,0 +1,4 @@
+lado = float(input('digite o tamanho do lado do quadrado :'))
+areaQuadrado = (lado**2)
+dobrodaArea = (areaQuadrado * 2)
+print('O dobro da area do quadrado é {}'.format(dobrodaArea))
