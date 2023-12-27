a3315023df2315fc74f21375c9660612cd2c988b
pedrocaldas42 <64943773+pedrocaldas42@users.noreply.github.com>
2023-12-26 23:50:36 -0300

    Create 4 - Faça um Programa que peça as 4 notas bimestrais e mostre a média.py

---
+++ 4 - Faça um Programa que peça as 4 notas bimestrais e mostre a média.py
@@ -0,0 +1,6 @@
+nota1 = float(input('Informe o primeira nota :'))
+nota2 = float(input('Informe o segunda nota :'))
+nota3 = float(input('Informe o terceira nota :'))
+nota4 = float(input('Informe o quarta nota :'))
+media = (nota1+nota2+nota3+nota4)/4
+print('A Média das Notas é {}'.format(media))
