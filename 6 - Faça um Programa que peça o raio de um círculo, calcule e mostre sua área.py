8a72219c51495458b8b89e29330648896393802c
pedrocaldas42 <64943773+pedrocaldas42@users.noreply.github.com>
2023-12-26 23:52:40 -0300

    Create 6 - Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.py

---
+++ 6 - Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.py
@@ -0,0 +1,4 @@
+import math #importa função math.pi , que é o propio Pi
+raioCirculo = float(input('digite o raio do circulo :'))
+areaCirculo = math.pi*(raioCirculo**2)
+print('A área do circulo é {}'.format(areaCirculo))
